a8c528249d50c84d619d1b98bb1b20debce9ff43
Ayberk Gerey <ayberkgerey@gmail.com>
2022-05-11 16:14:25 +0300

    android fix

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './src/navigations/StackNavigation';
 import MarkProvider from './src/contexts/MarkContext';
 
 export default function App() {
+  useEffect(() => {
+    console.disableYellowBox = true;
+  }, []);
+
   return (
     <MarkProvider>
       <NavigationContainer>
--- android/app/src/main/java/com/spaceflights/MainApplication.java
+++ android/app/src/main/java/com/spaceflights/MainApplication.java
@@ -10,8 +10,9 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.InvocationTargetException; 
 import java.util.List;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,9 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
+
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
           return packages;
         }
 
